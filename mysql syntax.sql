b09952cec7632251d3bee8972bfce1feb2dab7a7
dhanupreeth <dhanupreeth@gmail.com>
2014-11-08 18:05:40 +0530

    Update mysql syntax.sql

--- mysql syntax.sql
+++ mysql syntax.sql
@@ -1,11 +1,16 @@
 
-Mysql Database side:
+--Mysql Database side:
 
-Need to create database and create table like xml data schema as shown below:
+--Need to create database and create table like xml data schema as shown below:
 
-Mysql syntax:
+--Mysql syntax:
 
 create database xml_to_mysql;
 use xml_to_mysql;
-create table Tags(Id int,TagName nvarchar(25),Count int,ExcerptPostId int, WikiPostId int);
+create table xml(Id int,TagName nvarchar(25),Count int,ExcerptPostId int, WikiPostId int);
+
+--now loading the xml file from /tmp/code location from my Linux server: 
+
+load xml local infile '/tmp/code/Tags.xml' into table xml ROWS IDENTIFIED By '<row>';
+
 \q;
